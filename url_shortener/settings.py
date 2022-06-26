4f8f319979fe3dcdd77415d8f9eac2176192d1cf
furkanozkaya <furkanozkaya45@gmail.com>
2022-06-26 14:26:06 +0300

    secret-key security fix

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-px1+^-o!ye*a+zralahd9!74))e%3=@u#2l5xnjsddb@$4gw*l'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
